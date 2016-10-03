9ffaa2c4c341058bfa86b23453a376e34af4c6ce
Evgenii_Lartcev <Evgenii_Lartcev@epam.com>
2016-10-03 17:18:10 +0300

    format code

--- src/main/java/advanced/MapWithFeautures.java
+++ src/main/java/advanced/MapWithFeautures.java
@@ -12,22 +12,21 @@ import java.awt.*;
  * @author Evgenii_Lartcev.
  */
 public class MapWithFeautures extends PApplet {
-
     //use JDK to getting a screen size
     private static final Toolkit TOOLKIT = Toolkit.getDefaultToolkit();
     private static final Dimension SCREEN_SIZE = TOOLKIT.getScreenSize();
 
-    private static final int SCREEN_WIDTH =  SCREEN_SIZE.width;
+    private static final int SCREEN_WIDTH = SCREEN_SIZE.width;
     private static final int SCREEN_HEIGHT = SCREEN_SIZE.height;
 
-    private static final int SCREEN_WIDTH_FOR_APP = SCREEN_WIDTH - SCREEN_WIDTH /10;
-    private static final int SCREEN_HEIGHT_FOR_APP = SCREEN_HEIGHT - SCREEN_HEIGHT /10;
+    private static final int SCREEN_WIDTH_FOR_APP = SCREEN_WIDTH - SCREEN_WIDTH / 10;
+    private static final int SCREEN_HEIGHT_FOR_APP = SCREEN_HEIGHT - SCREEN_HEIGHT / 10;
 
-    private static final int START_POINT_ON_X_FOR_MAP = SCREEN_WIDTH /7;
-    private static final int START_POINT_ON_Y_FOR_MAP = SCREEN_HEIGHT /20;
+    private static final int START_POINT_ON_X_FOR_MAP = SCREEN_WIDTH / 7;
+    private static final int START_POINT_ON_Y_FOR_MAP = SCREEN_HEIGHT / 20;
 
-    private static final int SCREEN_WIDTH_FOR_MAP = SCREEN_WIDTH_FOR_APP - SCREEN_WIDTH_FOR_APP /5;
-    private static final int SCREEN_HEIGHT_FOR_MAP = SCREEN_HEIGHT_FOR_APP - SCREEN_HEIGHT_FOR_APP /15;
+    private static final int SCREEN_WIDTH_FOR_MAP = SCREEN_WIDTH_FOR_APP - SCREEN_WIDTH_FOR_APP / 5;
+    private static final int SCREEN_HEIGHT_FOR_MAP = SCREEN_HEIGHT_FOR_APP - SCREEN_HEIGHT_FOR_APP / 15;
 
     private UnfoldingMap map;
 
@@ -52,7 +51,6 @@ public class MapWithFeautures extends PApplet {
     }
 
     /**
-     *
      * @param args name of classes that have to start
      */
     public static void main(String[] args) {

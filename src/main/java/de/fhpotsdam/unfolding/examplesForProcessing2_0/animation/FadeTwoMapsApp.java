28afc915878dc16df0417003663f3826d02bea9e
Evgenii_Lartcev <Evgenii_Lartcev@epam.com>
2016-10-04 13:33:45 +0300

    improve

--- src/main/java/de/fhpotsdam/unfolding/examplesForProcessing2_0/animation/FadeTwoMapsApp.java
+++ src/main/java/de/fhpotsdam/unfolding/examplesForProcessing2_0/animation/FadeTwoMapsApp.java
@@ -1,5 +1,6 @@
 package de.fhpotsdam.unfolding.examplesForProcessing2_0.animation;
 
+import de.fhpotsdam.unfolding.providers.EsriProvider;
 import processing.core.PApplet;
 import de.fhpotsdam.unfolding.UnfoldingMap;
 import de.fhpotsdam.unfolding.geo.Location;
@@ -28,10 +29,9 @@ public class FadeTwoMapsApp extends PApplet {
     private static int fadeMin = 0;
     private static int fadeMax = 255;
 
-
+    @Override
     public void settings() {
         size(WIDTH, HEIGHT, JAVA2D);
-
         // Set the position and size of our two maps.
         final int mapXposition = 0;
         final int mapYposition = 30;
@@ -40,19 +40,30 @@ public class FadeTwoMapsApp extends PApplet {
         // Set our location of the maps
         final float lon = 52.5f;
         final float lat = 13.4f;
-
         // Initialize two maps
-        map1 = new UnfoldingMap(this, mapXposition, mapYposition, mapWidth, mapHeight);
+        map1 = new UnfoldingMap(this, mapXposition, mapYposition, mapWidth, mapHeight, new EsriProvider.NatGeoWorldMap());
         map1.zoomAndPanTo(ZOOM, new Location(lon, lat));
         map2 = new UnfoldingMap(this, mapXposition, mapYposition, mapWidth, mapHeight, new Microsoft.AerialProvider());
         map2.zoomAndPanTo(ZOOM, new Location(lon, lat));
         MapUtils.createDefaultEventDispatcher(this, map1, map2);
     }
 
+    @Override
     public void draw() {
         background(0);
-
         // Calculate Fade Value
+        calculateFadeValue();
+        // Draw maps
+        tint(255);
+        map1.draw();
+        tint(255, fadeVal);
+        map2.draw();
+        // Description at the Top
+        fill(255);
+        text("Press key '1' to fade once   |   Press key '2' to fade always", 10, 20);
+    }
+
+    private static void calculateFadeValue() {
         if (fadeAlways) {
             fadeOnce = false;
             if (fadeVal == 0 || fadeVal == 255)
@@ -67,18 +78,9 @@ public class FadeTwoMapsApp extends PApplet {
             }
             fadeVal += fadeDelta;
         }
-
-        // Draw maps
-        tint(255);
-        map1.draw();
-        tint(255, fadeVal);
-        map2.draw();
-
-        // Description at the Top
-        fill(255);
-        text("Press key '1' to fade once   |   Press key '2' to fade always", 10, 20);
     }
 
+    @Override
     public void keyPressed() {
         switch (key) {
             case '1':
@@ -93,6 +95,6 @@ public class FadeTwoMapsApp extends PApplet {
     }
 
     public static void main(String[] args) {
-        PApplet.main(new String[]{"de.fhpotsdam.unfolding.examplesForProcessing2_0.animation.FadeTwoMapsApp"});
+        PApplet.main(new String[]{FadeTwoMapsApp.class.getName()});
     }
 }
